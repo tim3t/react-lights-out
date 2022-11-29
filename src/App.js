68013736c32f848a86bebd206a6220a7ca244e12
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-11-29 14:00:50 -0500

    completed app, going to do some styling

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Board from './Board';
+import Cell from './Cell';
+import './App.css';
+
+/** Simple app that just shows the LightsOut game. */
+
+function App() {
+	return (
+		<div className='App'>
+			<Board />
+		</div>
+	);
+}
+
+export default App;
